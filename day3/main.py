39845fee9b5522a72827c85c82d6013b5f783fd6
quitran1303 <quitran1303@gmail.com>
2023-09-19 17:33:38 +1000

    day3

---
+++ day3/main.py
@@ -0,0 +1,16 @@
+weight = float(input("Input your weight: "))
+height = float(input("Input your height:"))
+
+bmi = weight / (height**2)
+
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you are normal weight")
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, you are overweight")
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, you are obese")
+else: 
+    print(f"Your bmi is {bmi}, you are clinically obese")
+
